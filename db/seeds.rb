aea6f033f88ce8ac5a495e507d0ab1660f21a1a8
Timothy Angus <timangus321@gmail.com>
2022-01-20 11:51:23 +0100

    add scaffold, db and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creating a few products"
+
+Product.create!(
+    title: "Mug",
+    description: "A nice mug for your coffee, tea, hot chocolate and more. You can't go wrong with a mug!",
+    image: "https://images.pexels.com/photos/4065905/pexels-photo-4065905.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    price: 10.00
+)
+
+Product.create!(
+    title: "Knife",
+    description: "A nice solid knife to cut, chop and dice!",
+    image: "https://images.pexels.com/photos/4226864/pexels-photo-4226864.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    price: 3.00
+)
+
+Product.create!(
+    title: "Plate",
+    description: "Make sure to plate your dish nicely and use this fantastic plate to do it!",
+    image: "https://images.pexels.com/photos/563067/pexels-photo-563067.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    price: 15.00
+)
+
+puts "finished creating example products"
